bcddfabfb54b1f06746711afb8afa6191a7ff695
Jon K <Johnakao@gmail.com>
2021-04-26 15:47:13 -0700

    Complete Basics

---
+++ stack.js
@@ -0,0 +1,60 @@
+/** Node: node for a stack. */
+
+class Node {
+  val = null;
+  next = null;
+
+  constructor(val) {
+    this.val = val;
+  }
+}
+
+/** Stack: chained-together nodes where you can
+ *  remove from the top or add to the top. */
+
+class Stack {
+  top = null;
+  size = 0;
+
+  /** push(val): add new value to the top of the stack. Returns undefined. */
+
+  push(val) {
+    let newNode = new Node(val);
+
+    if (this.top === null) {
+      this.top = newNode;
+    } else {
+      let temp = this.top;
+      this.top = newNode;
+      this.top.next = temp;
+    }
+    this.size++;
+  }
+
+  /** pop(): remove the node from the top of the stack
+   * and return its value. Should throw an error if the stack is empty. */
+
+  pop() {
+    if (this.size === 0) {
+      throw new Error("Nope.");
+    }
+    let temp = this.top;
+    this.top = this.top.next;
+    this.size--;
+    return temp.val;
+  }
+
+  /** peek(): return the value of the top node in the stack. */
+
+  peek() {
+    return this.top.val;
+  }
+
+  /** isEmpty(): return true if the stack is empty, otherwise false */
+
+  isEmpty() {
+    return this.size === 0;
+  }
+}
+
+module.exports = Stack;
